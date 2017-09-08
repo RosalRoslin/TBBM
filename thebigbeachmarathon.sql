c0d3f9b1f87d667fbd2c8f7fc394787872ec8222
orbiten-pradeep <pradeep.j@orbiten.in>
2017-09-08 13:15:50 +0530

    Latest DB with Payu money

--- thebigbeachmarathon.sql
+++ thebigbeachmarathon.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.0.10.18
+-- https://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 31, 2017 at 05:24 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost:3306
+-- Generation Time: Sep 08, 2017 at 01:44 AM
+-- Server version: 5.6.36-cll-lve
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,12 +23,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `contactid` int(11) NOT NULL,
+  `firstname` varchar(300) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `website` varchar(100) NOT NULL,
+  `message` varchar(400) NOT NULL,
+  `msgtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`contactid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`contactid`, `firstname`, `email`, `website`, `message`, `msgtime`) VALUES
+(0, 'Mickey', 'vidhimangal.v@gmail.com', '987654321', 'Hiiiiiiii', '2017-09-08 05:51:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `transactions_id` int(11) DEFAULT NULL,
+  `txnid` varchar(255) DEFAULT NULL,
+  `tickets` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `tshirt` varchar(255) DEFAULT NULL,
+  `KM` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `transactions`
 --
 
 CREATE TABLE IF NOT EXISTS `transactions` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NOT NULL,
+  `users_id` int(11) DEFAULT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `txnid` varchar(255) DEFAULT NULL,
@@ -62,21 +107,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Height` varchar(255) DEFAULT NULL,
   `Weight` varchar(255) DEFAULT NULL,
   `Membership` varchar(255) DEFAULT NULL,
-  `blood` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  `groups` varchar(255) DEFAULT NULL,
   `userid` varchar(255) DEFAULT NULL,
-  `profile` varchar(255) DEFAULT NULL,
+  `profiile` varchar(255) DEFAULT NULL,
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`ID`, `FirstName`, `LastName`, `email`, `password`, `DOB`, `Gender`, `ContactNumber`, `Height`, `Weight`, `Membership`, `blood`, `role`, `groups`, `userid`, `profile`, `created`) VALUES
-(6, 'Pradeep', 'J', 'pradeep@orbiten.in', '$2y$10$OXYSrDBQSVifvolPRQ4kqORs86vF23mJKO1b9yDA.1fGr3yz84LG6', '0000-00-00', 'Male', '9677859889', '', '', NULL, 'A+', 'User', 'Bangalore', NULL, 'profile_08-30-2017_0302pm.jpg', '2017-08-18 21:03:55');
+INSERT INTO `users` (`ID`, `FirstName`, `LastName`, `email`, `password`, `DOB`, `Gender`, `ContactNumber`, `Height`, `Weight`, `Membership`, `userid`, `profiile`, `created`) VALUES
+(7, 'Pradeep', 'Jeyachandran', 'pradeep@orbiten.in', '$2y$10$Meu2U..8kXhnjnfraXjPDuo4f4GheGBvonmwNhfosbUaZh8OSX2Gu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-08-18 10:35:13'),
+(8, 'vasanthaprakash', 'chandrasekaran', 'vasanth1407@gmail.com', '$2y$10$QByBHvuLg6VzQFex6ASCg.xZit5lC9pbDRm6BUNh5.44MS0sFZ/vy', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-08-21 23:26:58'),
+(9, 'lakshmi narasimhan', 'N', 'nl.adarsh@gmail.com', '$2y$10$8wBslw9ie8ssnqlPYzVzDOSoqIEr5fV0oRmKSKmnv5WDLzYG9CoYC', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-09-01 08:43:25'),
+(10, 'Roslin', 'a', 'roslin.albert@gmail.com', '$2y$10$F8U5LXoBFyfP0vQg2sJAr.gZvgSgKM6FssGTLhonL0qYjKQZBqGi6', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-09-06 04:13:21');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
